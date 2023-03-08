96dde2a565a2eb81cb114f7b3b99f2f13e5627b9
pham duy <phd051199@gmail.com>
2023-03-08 15:57:11 +0700

    Fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 config();
 
 export const PROXY_PORT = process.env.PROXY_PORT || 8001;
-export const WEB_INTERFACE_PORT = process.env.WEB_INTERFACE_PORT || 8002;
+export const WEB_INTERFACE_PORT = process.env.WEB_INTERFACE_PORT;
